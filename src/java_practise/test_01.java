0db04a4849641ec645b609d60d083b900b8f4fa4
MrsClaire <1129677497@qq.com>
2017-12-20 19:48:26 +0800

    日常更新...

---
+++ src/java_practise/test_01.java
@@ -0,0 +1,25 @@
+package java_practise;
+
+import java.util.Scanner;
+
+public class test_01{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        Solution solution = new Solution();
+        System.out.println(solution.Add(num1, num2));
+    }
+}
+class Solution {
+    public int Add(int num1,int num2) {
+        while(num2 != 0){
+            int temp = num1^num2;
+            num2 = (num1&num2)<<1;
+            num1 = temp;
+        }
+        return num1;
+    }
+}
+
+
