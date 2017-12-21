5431fa0cbc95bf7b51346d25bf23f834b14c7560
MrsClaire <1129677497@qq.com>
2017-12-21 21:57:19 +0800

    日常更新...

--- src/java_practise/Test_02.java
+++ src/java_practise/Test_02.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 // 每一列都按照从上到下递增的顺序排序。
 // 请完成一个函数，输入这样的一个二维数组和一个整数，
 // 判断数组中是否含有该整数
-
+// https://www.nowcoder.com/practice/abc3fe2ce8e146608e868a70efebf62e?tpId=13&tqId=11154&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
 //从左下角开始查找
 public class Test_02 {
     public static void main(String[] args){
