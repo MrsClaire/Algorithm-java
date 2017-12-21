d2cfa8c07d29a984a2ca9bf0a3bbfc115ed757f6
MrsClaire <1129677497@qq.com>
2017-12-21 21:56:35 +0800

    日常更新...

--- src/java_practise/Test_02.java
+++ src/java_practise/Test_02.java
@@ -10,25 +10,25 @@ import java.util.Scanner;
 //从左下角开始查找
 public class Test_02 {
     public static void main(String[] args){
-        int[][] array={{1,2,3,5,7,8,9,10,11},
-                        {2,3,4,5,8,10,11,13,15},
-                        {3,4,5,6,10,11,14,18,20},
-                        {5,6,8,10,15,18,20,25,29},
-                        {7,10,20,27,36,41,47,50,56},
-                        {10,12,23,29,48,57,63,77,99}};
-        System.out.println(find(20,array));
-        System.out.println(find(19,array));
+        int[][] array = {{1, 2, 3, 5, 7, 8, 9, 10, 11},
+                        {2, 3, 4, 5, 8, 10, 11, 13, 15},
+                        {3, 4, 5, 6, 10, 11, 14, 18, 20},
+                        {5, 6, 8, 10, 15, 18, 20, 25, 29},
+                        {7, 10, 20, 27, 36, 41, 47, 50, 56},
+                        {10, 12, 23, 29, 48, 57, 63, 77, 99}};
+        System.out.println(find(20, array));
+        System.out.println(find(19, array));
     }
     public static boolean find(int target, int [][] array) {
         int m = array.length;//行
         int n = array[0].length;//列
 
-        int x=m-1;  //行
-        int y=0;    //列
-        while(x>=0 && y<=n-1){
-            if(target<array[x][y]){
+        int x = m-1;  //行
+        int y = 0;    //列
+        while(x >= 0 && y <= n - 1){
+            if(target < array[x][y]){
                 x--;//上移1行
-            }else if(target>array[x][y]){
+            }else if(target > array[x][y]){
                 y++;//右移1列
             }else {//查到
                 return true;
